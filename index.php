2e106918383971fd2dd195c3426af88c50ba02de
Davon Morris <davm2142@gmail.com>
2025-03-25 18:43:32 -0500

    index change

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-declare(strict_types = 1);
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+// declare(strict_types = 1);
+// header('Access-Control-Allow-Origin: *');
+// header('Content-Type: application/json');
 $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method === 'OPTIONS') {
@@ -20,4 +20,12 @@ if ($method === 'OPTIONS') {
     phpinfo();   
 }
     echo '<h1>INF653 Midterm Project - Davon Morris</h1>';
+    echo '<h2>Links to the API options:</h2>';
+    echo '
+        <ul>
+            <a href ="/api/quotes" target=blank><li>Quotes</li></a>
+            <a href ="/api/authors" target=blank><li>Authors</li></a>
+            <a href ="/api/categories" target=blank><li>Categories</li></a>
+        </ul>
+    ';
 
